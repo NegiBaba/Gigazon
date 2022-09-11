867059e8c0faea832ca6e3519c7aa9e789842529
NegiBaba <piyushnegi319@gmail.com>
2022-09-11 22:07:50 +0530

    Added different routes

--- src/App.js
+++ src/App.js
@@ -1,3 +1,4 @@
+import { Box } from '@mui/material'
 import React, { useState } from 'react'
 import { BrowserRouter, Routes, Route, Navigate } from 'react-router-dom'
 import NavBar from './components/navbar'
@@ -9,11 +10,17 @@ function App() {
   return (
     <BrowserRouter>
       <NavBar currentPage={currentPage} redirectToPage={setCurrentPage} />
-      <Routes>
-        <Route path="/" element={<Navigate replace to="/home" />} />
-        <Route path="/home" element={<Home />} />
-        <Route path="/products" element={<Products />} />
-      </Routes>
+      <Box
+        sx={{
+          p: 3
+        }}
+      >
+        <Routes>
+          <Route path="/" element={<Navigate replace to="/home" />} />
+          <Route path="/home" element={<Home />} />
+          <Route path="/products" element={<Products />} />
+        </Routes>
+      </Box>
     </BrowserRouter>
   )
 }
