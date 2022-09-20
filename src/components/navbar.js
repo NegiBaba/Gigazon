2ac490b372b783eb9bd916700b9696efcd745605
NegiBaba <piyushnegi319@gmail.com>
2022-09-21 00:50:54 +0530

    added redux feature to navbar

--- src/components/navbar.js
+++ src/components/navbar.js
@@ -3,19 +3,30 @@ import Grid from '@mui/material/Unstable_Grid2'
 import SearchOutlinedIcon from '@mui/icons-material/SearchOutlined'
 import ShoppingCartOutlinedIcon from '@mui/icons-material/ShoppingCartOutlined'
 import AccountCircleIcon from '@mui/icons-material/AccountCircle'
-import React, { useEffect } from 'react'
+import React, { useEffect, useState } from 'react'
 import NavLink from './navlink'
 import PropTypes from 'prop-types'
 import { useLocation } from 'react-router-dom'
+import { useDispatch, useSelector } from 'react-redux'
+import { fetchCartItems } from '../features/cartSlice'
 
 const NavBar = ({ currentPage, redirectToPage }) => {
   const pages = ['Home', 'Products', 'Orders', 'Discontinued']
 
   const location = useLocation()
+  const dispatch = useDispatch()
+  const cart = useSelector((state) => state.cart)
+
+  const [cartItemsCount, setCartItemsCount] = useState(0);
 
   useEffect(() => {
     redirectToPage(location.pathname.slice(1))
-  })
+    if (cart.status === 'idle') {
+      dispatch(fetchCartItems());
+      setCartItemsCount(cart.products.length);
+    }
+    setCartItemsCount(cart.products.length)
+  }, [cart, dispatch])
 
   return (
     <Box
@@ -74,7 +85,7 @@ const NavBar = ({ currentPage, redirectToPage }) => {
                 }}
               >
                 <SearchOutlinedIcon />
-                <Badge badgeContent={1} color="secondary">
+                <Badge badgeContent={cartItemsCount} color="secondary">
                   <ShoppingCartOutlinedIcon />
                 </Badge>
                 <AccountCircleIcon />
