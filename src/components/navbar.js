c22725dc851d76b454c4066e423c0aa3475b0747
NegiBaba <piyushnegi319@gmail.com>
2022-09-24 13:04:27 +0530

    Removed navlinks and added search bar with category dropdown

--- src/components/navbar.js
+++ src/components/navbar.js
@@ -1,23 +1,25 @@
-import { Badge, Box, Container, Typography } from '@mui/material'
+import { Badge, Box, Container, InputBase, Typography } from '@mui/material'
 import Grid from '@mui/material/Unstable_Grid2'
 import SearchOutlinedIcon from '@mui/icons-material/SearchOutlined'
 import ShoppingCartOutlinedIcon from '@mui/icons-material/ShoppingCartOutlined'
 import AccountCircleIcon from '@mui/icons-material/AccountCircle'
 import React, { useEffect, useState } from 'react'
-import NavLink from './navlink'
+// import NavLink from './navlink'
 import PropTypes from 'prop-types'
 import { useLocation } from 'react-router-dom'
 import { useDispatch, useSelector } from 'react-redux'
 import { fetchCartItems } from '../features/cartSlice'
+import EggIcon from '@mui/icons-material/Egg';
+import KeyboardArrowDownIcon from '@mui/icons-material/KeyboardArrowDown';
 
 const NavBar = ({ currentPage, redirectToPage }) => {
-  const pages = ['Home', 'Products', 'Orders', 'Discontinued']
 
   const location = useLocation()
   const dispatch = useDispatch()
   const cart = useSelector((state) => state.cart)
 
   const [cartItemsCount, setCartItemsCount] = useState(0);
+  const [showCategories, setCategoriesDropwdownState] = useState(false);
 
   useEffect(() => {
     redirectToPage(location.pathname.slice(1))
@@ -27,6 +29,21 @@ const NavBar = ({ currentPage, redirectToPage }) => {
     }
     setCartItemsCount(cart.products.length)
   }, [cart, dispatch])
+
+
+  const categoriesDropdown = () => (
+    <Box sx={{
+      border: '1px solid red',
+      left: '0',
+      position: 'absolute',
+      top: '50px',
+      transform: 'translateX(-25%)',
+      height: '300px',
+      width: '300px'
+    }}>
+      dropdown
+    </Box>
+  )
 
   return (
     <Box
@@ -41,10 +58,17 @@ const NavBar = ({ currentPage, redirectToPage }) => {
             container
             sx={{
               alignItems: 'center',
-              height: '60px'
+              py: '30px'
             }}
           >
-            <Grid Item lg={2}>
+            <Grid Item lg={2} sx={{
+              alignItems: 'center',
+              display: 'flex',
+            }}>
+              <EggIcon sx={{
+                color: 'secondary.main',
+                fontSize: '2.4rem'
+              }} />
               <Typography
                 variant="h5"
                 sx={{
@@ -64,14 +88,54 @@ const NavBar = ({ currentPage, redirectToPage }) => {
                 justifyContent: 'center'
               }}
             >
-              {pages.map((page) => (
-                <NavLink
-                  key={page}
-                  label={page}
-                  isActive={page === currentPage}
-                  redirectToPage={() => redirectToPage(page)}
-                />
-              ))}
+
+              {/* Search Bar */}
+              <Box sx={{
+                alignItems: 'center',
+                backgroundColor: 'background.secondary',
+                borderRadius: '5px',
+                display: 'flex',
+              }}>
+
+                {/* Dropdown Box */}
+                <Box
+                  onClick={() => setCategoriesDropwdownState(!showCategories)}
+                  sx={{
+                    alignItems: 'center',
+                    cursor: 'pointer',
+                    display: 'flex',
+                    p: '14px 20px',
+                    position: 'relative'
+                  }}>
+                  <Typography variant='subtitle2' sx={{
+                    fontWeight: 400,
+                    pr: '10px',
+                    textTransform: 'uppercase',
+                  }}>
+                    All Categories
+                  </Typography>
+                  <KeyboardArrowDownIcon />
+                  {showCategories ? categoriesDropdown() : null}
+                </Box>
+
+                <InputBase
+                  placeholder={`I'm looking for....`}
+                  sx={{
+                    borderLeft: `1px solid rgba(0, 0, 0, 0.2)`,
+                    fontSize: '13px',
+                    minWidth: '300px',
+                    px: '10px'
+                  }}></InputBase>
+
+                <Box sx={{
+                  display: 'flex',
+                  cursor: 'pointer',
+                  px: '20px'
+                }}>
+                  <SearchOutlinedIcon />
+                </Box>
+
+              </Box>
             </Grid>
             <Grid Item lg={2}>
               <Box
@@ -93,8 +157,8 @@ const NavBar = ({ currentPage, redirectToPage }) => {
             </Grid>
           </Grid>
         </Box>
-      </Container>
-    </Box>
+      </Container >
+    </Box >
   )
 }
 
