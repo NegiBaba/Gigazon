7b85e892bcf531d6c8b401b4270d8d7a83d26261
NegiBaba <piyushnegi319@gmail.com>
2022-10-01 23:38:07 +0530

    Using component for filter inside submenu

--- src/components/submenu.js
+++ src/components/submenu.js
@@ -1,8 +1,58 @@
-import { Box } from '@mui/material';
-import React from 'react';
+import { Box, Container } from '@mui/material'
+import Filter from './filter'
+import Grid from '@mui/material/Unstable_Grid2'
+import MenuIcon from '@mui/icons-material/Menu'
+import React, { useState } from 'react'
 
-const subMenu = () => {
-  return <Box>SubMenu</Box>
+const SubMenu = () => {
+  const cateforyList = [
+    {
+      id: '0',
+      name: 'All Categories'
+    },
+    {
+      id: '1',
+      name: 'Food'
+    },
+    {
+      id: '2',
+      name: 'Drink'
+    },
+    {
+      id: '3',
+      name: 'Fruit'
+    }
+  ]
+
+  const [selectedCategory, setSelectedCategory] = useState({
+    id: '1',
+    name: 'All Categories'
+  })
+
+  return (
+    <Box
+      sx={{
+        border: '1px solid',
+        borderColor: 'primary.dark',
+        borderWidth: '0 0 1px',
+        p: 1
+      }}
+    >
+      <Container>
+        <Grid container>
+          <Grid item lg={3}>
+            <Filter
+              withIcon={true}
+              StartIcon={MenuIcon}
+              list={cateforyList}
+              selected={selectedCategory}
+              selectFilterItem={(category) => setSelectedCategory(category)}
+            />
+          </Grid>
+        </Grid>
+      </Container>
+    </Box>
+  )
 }
 
-export default subMenu;
+export default SubMenu
